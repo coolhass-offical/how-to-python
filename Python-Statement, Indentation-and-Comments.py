d67fc7b6f7271882e1d65d17abef85d1a8212aa1
coolhass-offical <blyatmaxchiu@gmail.com>
2020-09-02 18:02:01 +0800

    Python Statement, Indentation and Comments

---
+++ Python-Statement, Indentation-and-Comments.py
@@ -0,0 +1,27 @@
+# Instructions that a Python interpreter can execute are called statements.
+a = 1
+
+# In Python, the end of a statement is marked by a newline character.
+# But we can make a statement extend over multiple lines with the line continuation character (\).
+b = 1 + 2 + 3 +\
+    3 + 4 + 5 +\
+    5 + 6 + 7
+
+# In Python, line continuation is implied inside parentheses ( ), brackets [ ], and braces { }.
+c = (1 + 2 +
+     3 + 4)
+
+# The surrounding parentheses ( ) do the line continuation implicitly. Same is the case with [ ] and { }.
+colors = ['green',
+          'red',
+          'blue']
+
+# We can also put multiple statements in a single line using semicolons.
+d = 1
+e = 2
+f = 3
+# ps: vscode putts them in order automaticly.
+# like this
+g = 1
+h = 2
+i = 3
