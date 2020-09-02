bfc61f1a35d69f48ed01e8c5f94045555e7e377e
coolhass-offical <blyatmaxchiu@gmail.com>
2020-09-02 18:45:31 +0800

    indentation and comments

--- Python-Indentation-and-comments.py
+++ Python-Indentation-and-comments.py
@@ -1,7 +1,17 @@
 # A code block (body of a function, loop, etc.) starts with indentation and ends with the first unindented line.
 # The amount of indentation is up to you, but it must be consistent throughout that block.
 
+if True:
+    print('Hello')
+    a = 5
+# The enforcement of indentation in Python makes the code look neat and clean.
+# This results in Python programs that look similar and consistent.
 for i in range(1, 11):
     print(i)
     if i == 5:
         break
+"""this is another way to write a comment"""
+
+"""you can also
+do it like
+this"""
