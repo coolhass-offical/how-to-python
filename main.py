cd7633010cd824dcb757d708eaab3c8e295391f9
coolhass-offical <blyatmaxchiu@gmail.com>
2020-09-03 18:16:52 +0800

    Variables

---
+++ main.py
@@ -0,0 +1,8 @@
+import constant
+
+print(constant.PI)
+print(constant.GRAVITY)
+
+"""Note: In reality, we don't use constants in Python. 
+Naming them in all capital letters is a convention to separate them from variables, 
+however, it does not actually prevent reassignment."""
